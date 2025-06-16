ea93a9822a0f10ae0a2413274e9fd8af44b6291f
Nicalpesh31 <mca2021.alpeshnichat@asmedu.org>
2025-06-16 23:52:11 +0530

    update

---
+++ Razerpay website/src/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        razorpayDark: "#02042a",
+        razorpayBlue: "#2b84ea",
+        lightText: "#818597",
+      },
+    },
+  },
+  plugins: [],
+};
